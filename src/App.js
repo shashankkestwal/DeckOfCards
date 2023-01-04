3ad02f6d8fb615f425656877c1a17617dc2b0209
shashankkestwal <shashankkestwal777@gmail.com>
2023-01-04 18:33:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Deck from "./Deck.js"
+function App() {
+  return (
+    <div className="App">
+      <Deck />
+    </div>
+  );
+}
+
+export default App;
